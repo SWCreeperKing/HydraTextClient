084bc0e6d4dbe8a61ceb89ff389a58b5ab4fad27
SWCreeperKing <SW.CreeperKing@gmail.com>
2025-06-25 13:02:25 -0700

    Moved more

--- Scripts/HintTab/HintDragable.cs
+++ Scripts/HintTab/HintDragable.cs
@@ -2,7 +2,28 @@ using Godot;
 
 namespace ArchipelagoMultiTextClient.Scripts.HintTab;
 
-public partial class HintDragable : Node
+public partial class HintDragable : PanelContainer
 {
+    public static bool IsDragging;
     
+    public override void _DropData(Vector2 atPosition, Variant data)
+    {
+    }
+
+    public override bool _CanDropData(Vector2 atPosition, Variant data)
+    {
+        return true;
+    }
+
+    public override Variant _GetDragData(Vector2 atPosition)
+    {
+        IsDragging = true;
+        return 1;
+    }
+
+    public override void _Notification(int what)
+    {
+        if (what != NotificationDragEnd) return;
+        IsDragging = false;
+    }
 }
--- Scripts/HintTab/HintOrganizer.cs
+++ Scripts/HintTab/HintOrganizer.cs
@@ -5,4 +5,32 @@ namespace ArchipelagoMultiTextClient.Scripts.HintTab;
 public partial class HintOrganizer : HSplitContainer
 {
     [Export] private PanelContainer IndexFollower;
+    [Export] private VBoxContainer ListContainer;
+
+    public override void _Process(double delta)
+    {
+        var mouse = GetGlobalMousePosition();
+        var indexRect = IndexFollower.GetGlobalRect();
+        var isMouseOut = mouse.X + 10 < indexRect.Position.X;
+        IndexFollower.Visible = HintDragable.IsDragging && !isMouseOut;
+
+        if (!HintDragable.IsDragging) return;
+        if (isMouseOut) return;
+        if (indexRect.HasPoint(mouse)) return;
+
+        var childrenCount = ListContainer.GetChildren();
+        var index = childrenCount.IndexOf(IndexFollower);
+
+        if (index == childrenCount.Count - 1 && mouse.Y > indexRect.Position.Y) return;
+        if (index == 0 && mouse.Y < indexRect.Position.Y) return;
+
+        if (mouse.Y > indexRect.Position.Y)
+        {
+            ListContainer.MoveChild(IndexFollower, index + 1);
+        }
+        else
+        {
+            ListContainer.MoveChild(IndexFollower, index - 1);
+        }
+    }
 }
